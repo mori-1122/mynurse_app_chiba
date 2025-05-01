a774492707b54d9d991a07aee591476641b6fd0f
chiba <edyzrg1118@gmail.com>
2025-05-02 03:07:14 +0900

    Symptomsテーブルのカラムに制約を追加

--- db/migrate/20250428203823_create_symptoms.rb
+++ db/migrate/20250428203823_create_symptoms.rb
@@ -1,11 +1,11 @@
 class CreateSymptoms < ActiveRecord::Migration[8.0]
   def change
     create_table :symptoms do |t|
-      t.string :title
-      t.string :category
-      t.text :steps
+      t.string :title, null: false
+      t.string :category, null: false
+      t.text :steps, null: false
       t.text :points
-      t.boolean :should_see_doctor
+      t.boolean :should_see_doctor, default: false, null: false  #初期値false「基本的には様子見でOK」
       t.string :source_url
 
       t.timestamps
