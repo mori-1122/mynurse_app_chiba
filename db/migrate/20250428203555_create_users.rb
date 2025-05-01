149657550637aa24a1ebdf72c4224ca246389cfa
chiba <edyzrg1118@gmail.com>
2025-05-02 03:06:55 +0900

    Usersテーブルのカラムに制約を追加

--- db/migrate/20250428203555_create_users.rb
+++ db/migrate/20250428203555_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :encrypted_password
-      t.boolean :notification_enabled
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :encrypted_password, null: false
+      t.boolean :notification_enabled, default: true, null: false ##デフォルト値を設定
 
       t.timestamps
     end
