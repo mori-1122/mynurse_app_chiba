623789609fa7aca987a0b9eb861a95ad6c5de764
chiba <edyzrg1118@gmail.com>
2025-05-02 03:07:48 +0900

    Notificationsテーブルに制約を追加

--- db/migrate/20250428205450_create_notifications.rb
+++ db/migrate/20250428205450_create_notifications.rb
@@ -2,8 +2,8 @@ class CreateNotifications < ActiveRecord::Migration[8.0]
   def change
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :kind
-      t.boolean :active
+      t.string :kind, null: false
+      t.boolean :active, default: true, null: false ##新しく通知データを作成したとき、自動的にONになる
 
       t.timestamps
     end
