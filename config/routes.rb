6d65356c3ae33bf5f9876970678f7fb9067455dc
chiba <edyzrg1118@gmail.com>
2025-05-07 13:59:19 +0900

    Deviseのルーティングをカスタマイズし、ゲストログイン用ルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #  deviceを使ったユーザー認証
+  #  sessionsコントローラをオーバーライドし、自作ログイン処理（ゲスト対応など）を使えるようにする
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # ゲストログインを仮ユーザーとしてログインさせる
+  # guest_sign_in という新しいルート（POSTメソッド）を追加
+  # Users::SessionsControllerのguest_sign_inに遷移する
+  devise_scope :user do
+    post 'guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   get "home/index"
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index"
