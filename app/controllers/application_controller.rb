738573a4c6fc5494cd8bae6b19f2472062053c67
chiba <edyzrg1118@gmail.com>
2025-05-08 02:56:33 +0900

    Devise用のパラメータ許可処理を追加し、コメントを整理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base # すべてのコントローラの親クラス。アプリ全体の共通処理を書く場所。
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-#   allow_browser versions: :modern
+  #   allow_brows before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Deviseが使われているコントローラ（ログイン・サインアップなど）の時だけ、 configure_permitted_parameters メソッドを実行する。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # これ以降に書かれるメソッドは 継承先のコントローラからは使えるが、外部（URL）から呼び出せない。セキュリティ上の保護。
+  protected
+
+ # Deviseのリクエストに許可するパラメータ（フォームから受け取ってもOKなデータ）を追加設定するためのメソッド。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
+
+
+
+
+
